fde460b160aa7cc4654448de9521f43bd92abae8
theus2br <matlivebr@hotmail.com>
2019-11-16 13:28:50 -0300

    Buscando por ID

--- src/main/java/com/matdoli/geolocalization/controller/GeoController.java
+++ src/main/java/com/matdoli/geolocalization/controller/GeoController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,29 +12,38 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.matdoli.geolocalization.model.GeoEntidade;
 import com.matdoli.geolocalization.resources.model.GeoResource;
+import com.matdoli.geolocalization.service.GeoBuscarService;
 import com.matdoli.geolocalization.service.GeoCadastroServiceImpl;
 import com.matdoli.geolocalization.service.GeoServiceImpl;
 
 @RestController
-@RequestMapping(path ="/postgres")
+@RequestMapping(path = "/postgres")
 public class GeoController {
-	
+
 	@Autowired
 	private GeoServiceImpl buscarGeoImpl;
-	
+
 	@Autowired
 	private GeoCadastroServiceImpl cadastroService;
 	
+	@Autowired
+	private GeoBuscarService buscarService;
 	
+	
+
 	@GetMapping(path = "/geolocalizacao")
-	public List<GeoEntidade> buscarAllGeo(){
+	public List<GeoEntidade> buscarAllGeo() {
 		return buscarGeoImpl.buscarTodasGeo();
 	}
-	
-	 @PostMapping(path = "/geolocalizacao/save")
-	    public void salvarGeolocalizacao(
-	            @RequestBody GeoResource geolocalizacao) {
-		 cadastroService.cadastro(geolocalizacao);
-	    }
-}
 
+	@PostMapping(path = "/geolocalizacao/save")
+	public void salvarGeolocalizacao(@RequestBody GeoResource geolocalizacao) {
+		cadastroService.cadastro(geolocalizacao);
+	}
+	
+	@GetMapping(path = "/geolocalizacao/id/{id}")
+	public GeoEntidade buscarPorId(@PathVariable(name="id", required = true) Long id) {
+		return buscarService.buscarPorId(id);
+	}
+
+}
---
+++ src/main/java/com/matdoli/geolocalization/service/GeoBuscarService.java
@@ -0,0 +1,28 @@
+package com.matdoli.geolocalization.service;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.matdoli.geolocalization.model.GeoEntidade;
+import com.matdoli.geolocalization.repository.GeoRepository;
+
+@Service
+public class GeoBuscarService {
+
+	@Autowired
+	private GeoRepository geoRepository;
+	
+	public GeoEntidade buscarPorId(Long id) {
+		
+		Optional<GeoEntidade> OptionalGeo = geoRepository.findById(id);
+		GeoEntidade geo = new GeoEntidade();
+		
+		geo = OptionalGeo.get();
+		
+		return geo;
+	}
+	
+	
+}
